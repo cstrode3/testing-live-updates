1c22d6a618a6581022c55dc18d7807497fccd53e
Celeste Strode <142926065+cstrode3@users.noreply.github.com>
2024-11-14 16:43:33 -0600

    Including Template Files for basic Functionality

---
+++ get.js
@@ -0,0 +1,16 @@
+console.clear();
+
+async function getData() {
+    const url = "https://cstrode3.github.io/Website.txt";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
